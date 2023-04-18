94a1b48335b3862bf57c5e7546f156c42476d68c
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-04-18 17:38:41 +0900

    first commit

---
+++ src/main/java/com/likelion/javaproject2/PrintHello.java
@@ -0,0 +1,8 @@
+package com.likelion.javaproject2;
+
+public class PrintHello { // 클래스
+    public void print() { // 메소드
+        System.out.println("Hello"); // 명령
+    }
+
+}
---
+++ src/main/java/com/likelion/javaproject2/PrintHelloTest.java
@@ -0,0 +1,11 @@
+package com.likelion.javaproject2;
+
+public class PrintHelloTest {
+    public static void main(String[] args) {
+        PrintHello printHello = new PrintHello();
+        printHello.print();
+
+        PrintBye printBye = new PrintBye();
+        printBye.print();
+    }
+}
