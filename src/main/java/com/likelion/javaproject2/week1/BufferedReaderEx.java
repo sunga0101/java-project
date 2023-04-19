bc04641a3e90146814d84366125735c48d49ae15
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-04-19 14:59:16 +0900

    feat: add BufferedReader

---
+++ src/main/java/com/likelion/javaproject2/week1/BufferedReaderEx.java
@@ -0,0 +1,15 @@
+package com.likelion.javaproject2.week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReaderEx {
+    public void readALine() throws IOException {
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is); // 카트
+
+        System.out.println("line1 = "+ br.readLine());
+        System.out.println("line2 = "+ br.readLine()); // 두줄 입력하기
+    }
+}
