abe90be65a03d5aea0cc001ec348e70944595217
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-04-19 15:39:18 +0900

    feat: add ScannerEx

---
+++ src/main/java/com/likelion/javaproject2/week1/ScannerEx.java
@@ -0,0 +1,13 @@
+package com.likelion.javaproject2.week1;
+
+import java.util.Scanner;
+
+public class ScannerEx {
+    public void plusTwoNumbers(int val1, int val2) {
+    //
+    }
+    public void readTwoNumbersAndPlus() {
+        Scanner sc  = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
---
+++ src/main/java/com/likelion/javaproject2/week1/ScannerExTest.java
@@ -0,0 +1,10 @@
+package com.likelion.javaproject2.week1;
+
+public class ScannerExTest {
+    public static void main(String[] args) {
+
+        ScannerEx scannerEx = new ScannerEx();
+        scannerEx.readTwoNumbersAndPlus();
+
+    }
+}
