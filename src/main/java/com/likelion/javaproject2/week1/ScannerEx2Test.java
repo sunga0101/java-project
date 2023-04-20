626381d0a2dadd44481a9b50d165f03885cf89da
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-04-20 10:51:37 +0900

    feat: add ScannerEx2 & Test

---
+++ src/main/java/com/likelion/javaproject2/week1/ScannerEx2Test.java
@@ -0,0 +1,11 @@
+package com.likelion.javaproject2.week1;
+
+
+public class ScannerEx2Test {
+    public static void main(String[] args) {
+        ScannerEx2 scannerEx2 = new ScannerEx2();
+        scannerEx2.inputTwoNumbersAndPlus();
+    }
+}
+
+
