712fbfe8820cd829d88c39de89c193c8ff0d69d1
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-04-20 15:18:57 +0900

    feat: add VarialEx

---
+++ src/main/java/com/likelion/javaproject2/week1/day4/VarialEx.java
@@ -0,0 +1,17 @@
+package com.likelion.javaproject2.week1.day4;
+
+import com.likelion.javaproject2.PrintHello;
+
+public class VarialEx {
+    public static void main(String[] args) {
+        int iVal; // 변수 선언
+        iVal = 0; // 최초로 값을 지정. 초기화
+
+
+        PrintHello printHello; // 변수 선언
+        printHello = new PrintHello(); // 인스턴스화, 초기화 한번에.
+
+        System.out.println(iVal);
+        printHello.print();
+    }
+}
