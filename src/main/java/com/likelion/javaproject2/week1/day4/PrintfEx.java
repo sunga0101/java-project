2e01e0fcbfa598970c379c91f358e2b6fb1ecb97
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-04-20 13:19:34 +0900

    feat: add PrintfMultiTable

--- src/main/java/com/likelion/javaproject2/week1/day4/PrintfEx.java
+++ src/main/java/com/likelion/javaproject2/week1/day4/PrintfEx.java
@@ -1,4 +1,4 @@
-package com.likelion.javaproject2.week1;
+package com.likelion.javaproject2.week1.day4;
 
 public class PrintfEx {
     public void print() {
@@ -6,6 +6,11 @@ public class PrintfEx {
         System.out.println("bye");
         System.out.printf("%d * %d = %d\n", 2,2,4);
         System.out.println("2 "+"* "+"2 "+"= "+"4 ");
+
+        System.out.printf("%d\n", 4);
+        System.out.printf("%02d\n", 4);
+        System.out.printf("%02d\n", 11);
+        System.out.printf("%d-%02d-%02d", 2023, 4, 20);
     }
 
     public static void main(String[] args) {
