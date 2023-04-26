670776f777e31447edd121cd7cc47f753e3012b8
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-04-26 15:05:33 +0900

    [feat] add MinuteSecond

---
+++ src/main/java/com/likelion/javaproject2/week2/day3/MinuteSecond.java
@@ -0,0 +1,10 @@
+package com.likelion.javaproject2.week2.day3;
+
+public class MinuteSecond {
+    public static void main(String[] args) {
+        int seconds = 239;
+        int minute = seconds / 60 ;
+        int second = seconds % 60 ;
+        System.out.printf("%d분 %d초", minute, second);
+    }
+}
