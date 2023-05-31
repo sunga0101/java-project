5daf459a40785136c95245eb7207b0b5b29fde1a
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-05-31 14:04:28 +0900

    [feat] Person table id 자동증가, 메서드 분리

--- src/main/java/com/likelion/javaproject2/week7/day2/DBMain.java
+++ src/main/java/com/likelion/javaproject2/week7/day2/DBMain.java
@@ -7,27 +7,43 @@ import java.sql.*;
 public class DBMain extends BaseDAO {
     public static void main(String[] args) {
         DBMain dbMain = new DBMain();
-        dbMain.initPerson();
-        dbMain.getCodeName();
+//        dbMain.initPerson();
+        dbMain.insertPerson("!!");
+        dbMain.insertPerson("mini");
+        dbMain.insertPerson("jimin");
+    }
+    private int insertPerson(String name) {
+        int cnt = 0;
+        String sql = "insert into person(name) values(?)";
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            psmt.setString(1, name);
+            cnt = psmt.executeUpdate();
+        }
+        catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        finally {
+            close();
+        }
+        return cnt;
     }
 
     private void initPerson() {
-
+        int cnt = 0; // 몇 건이 변환되었는지
+        String sql = """
+                create table person (
+                id integer primary key autoincrement,
+                name string
+                )
+                """;
         try {
 
             getConn();
             smt = conn.createStatement();
-            smt.setQueryTimeout(30);  // set timeout to 30 sec.
-
             smt.executeUpdate("drop table if exists person");
-            smt.executeUpdate("create table person (id integer, name string)");
-            smt.executeUpdate("insert into person values(1, 'leo')");
-            smt.executeUpdate("insert into person values(2, 'yui')");
-            rs = smt.executeQuery("select * from person");
-            while(rs.next()) {
-                System.out.println("name = " + rs.getString("name"));
-                System.out.println("id = " + rs.getInt("id"));
-            }
+            cnt = smt.executeUpdate(sql);
         }
         catch(SQLException e) {
             System.err.println(e.getMessage());
