5ffa5a75b145221a5997b183fc2fa4d889141e67
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-06-21 10:49:46 +0900

    [feat] TreeTraverse 구현

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module com.likelion.javaproject2 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires junit;
-    requires org.junit.jupiter.api;
 
 
     opens com.likelion.javaproject2 to javafx.fxml;
